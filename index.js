ee39c50eca063260b308a73ff87adfd51b8bb915
Olowodev <nathanielolowofoyeku@gmail.com>
2025-03-10 18:01:32 +0000

    use render's port

--- index.js
+++ index.js
@@ -92,6 +92,8 @@ app.get('/', (req,res) => {
     res.send('testingggg')
 })
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
     console.log('Server is running on port 3000')
 })
