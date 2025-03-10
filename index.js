c875910499708e1930c11249e8a69a442fd2de2d
Olowodev <nathanielolowofoyeku@gmail.com>
2025-03-10 18:17:42 +0000

    testt

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.post('/login', async(req,res)=> {
             })
             .on('end', async () => {
                 console.log(user, 'end')
+                console.log(user.email.length)
                 if (user.email.length > 0) { 
                     console.log(user.email)  
                     // console.log(found)  
